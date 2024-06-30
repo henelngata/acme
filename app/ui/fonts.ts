70f51fc40e30a29fde7b8ef91f7723a420cbe4f4
henelngata <bucketpena013@gmail.com>
2024-06-30 12:01:13 +0300

    all

---
+++ app/ui/fonts.ts
@@ -0,0 +1,8 @@
+import { Inter, Lusitana } from "next/font/google";
+
+const lusitana = Lusitana({subsets: ["latin"], weight: ["400","700"]})
+const inter = Inter({ subsets: ['latin'] })
+export {
+    inter,
+    lusitana
+}
